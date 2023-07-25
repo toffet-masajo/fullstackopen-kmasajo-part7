b988a56d20165dec8e608dde8edbdb337209ac64
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2023-07-26 00:37:09 +0800

    7.1: routed anecdotes, step1

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,3 +1,4 @@
+import {Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react'
 
 const Menu = () => {
@@ -6,9 +7,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -126,9 +127,11 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+      </Routes>
       <Footer />
     </div>
   )
